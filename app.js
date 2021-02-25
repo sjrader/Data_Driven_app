e35783f048990b000e02b5a98e6d94100dbe00bc
Jason Slade <jason.slade906@gmail.com>
2021-02-25 10:58:27 -0700

    set up bin

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+const morgan = require('morgan');
 
 const app = express();
 
 app.set('view engine', 'pug');
 
+app.use(morgan('dev'));
+
 app.use(routes);
 
-const port = 8080;
-
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+module.exports = app;
